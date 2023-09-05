912a3232caee4b8ed0fab01d74e0ce45605ac4c9
Jacob Homanics <homanicsjake@gmail.com>
2023-09-04 23:09:27 -0500

    compilable tests - untested

--- packages/hardhat/test/YouContract.ts
+++ packages/hardhat/test/YouContract.ts
@@ -1,28 +1,84 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { YourContract } from "../typechain-types";
+import constants from "../scripts/constants";
+import { cutDiamond } from "../scripts/Diamond.cut";
+// import { deployDiamondCutFacet, deployFacet, deployDiamond, deployDiamondInit } from '../scripts/Diamond.deploy';
+// import { FacetCutAction } from 'hardhat-deploy/types';
+import { CustomERC1155, IDiamondCut } from "../typechain-types";
+import { deployDiamondCutFacet, deployFacet, deployDiamond, deployDiamondInit } from "../scripts/Diamond.deploy";
 
-describe("YourContract", function () {
-  // We define a fixture to reuse the same setup in every test.
+/**
+ * Utility function to make a facet cut on the diamond.
+ * @param diamondAddress diamond cut facet contract
+ * @param facetCuts an array of facet cuts to be made
+ * @param init address (?)
+ */
+// async function makeCut(
+//   signer: SignerWithAddress,
+//   diamondAddress: string,
+//   facetCuts: IDiamondCut.FacetCutStruct[],
+//   init: string,
+// ) {
+//   const diamondCutFacet = await ethers.getContractAt('DiamondCutFacet', diamondAddress, signer);
+//   const tx = await diamondCutFacet.diamondCut(facetCuts, init, '0x', {gasLimit: 800000});
+//   const receipt = await tx.wait();
+//   if (!receipt.status) {
+//     throw Error(`Diamond upgrade failed: ${tx.hash}`);
+//   }
+// }
 
-  let yourContract: YourContract;
+describe("Diamond", function () {
+  let owner: SignerWithAddress;
+  let diamondCutFacetAddress: string;
+  let diamondAddress: string;
+  let diamondInitAddress: string;
+  const facetCuts: IDiamondCut.FacetCutStruct[] = []; // each facet will be recorded here
+  const facetNameToAddress: Record<string, string> = {};
+
   before(async () => {
-    const [owner] = await ethers.getSigners();
-    const yourContractFactory = await ethers.getContractFactory("YourContract");
-    yourContract = (await yourContractFactory.deploy(owner.address)) as YourContract;
-    await yourContract.deployed();
+    [owner] = await ethers.getSigners();
   });
 
-  describe("Deployment", function () {
-    it("Should have the right message on deploy", async function () {
-      expect(await yourContract.greeting()).to.equal("Building Unstoppable Apps!!!");
+  describe("Diamond Deployment & Cut", async () => {
+    it("should deploy DiamondCutFacet", async () => {
+      diamondCutFacetAddress = await deployDiamondCutFacet();
     });
 
-    it("Should allow setting a new message", async function () {
-      const newGreeting = "Learn Scaffold-ETH 2! :)";
+    it("should deploy Diamond", async () => {
+      diamondAddress = await deployDiamond(owner.address, diamondCutFacetAddress);
+    });
 
-      await yourContract.setGreeting(newGreeting);
-      expect(await yourContract.greeting()).to.equal(newGreeting);
+    it("should deploy DiamondInit", async () => {
+      diamondInitAddress = await deployDiamondInit();
+    });
+
+    // deploy facets
+    constants.FacetNames.forEach(facetName =>
+      it("should deploy " + facetName, async () => {
+        const facetCut = await deployFacet(facetName);
+        facetCuts.push(facetCut);
+        facetNameToAddress[facetName] = facetCut.facetAddress as string;
+      }),
+    );
+
+    // cut the diamond to register facets at the diamond
+    // test each facet independently from that point on
+    it("should upgrade Diamond via DiamondCutFacet", async () => {
+      await cutDiamond(diamondAddress, diamondInitAddress, facetCuts);
+    });
+  });
+
+  describe("CustomERC1155", async () => {
+    let ownershipFacet: CustomERC1155;
+    before(async () => {
+      // sometimes, you dont' even need to cast typechain overloads the getContractAt within hardhat
+      ownershipFacet = await ethers.getContractAt("CustomERC1155", diamondAddress);
+    });
+
+    it("should have the correct owner", async () => {
+      const [owner] = await ethers.getSigners();
+      expect(await ownershipFacet.owner()).to.be.eq(owner.address);
     });
   });
 });
