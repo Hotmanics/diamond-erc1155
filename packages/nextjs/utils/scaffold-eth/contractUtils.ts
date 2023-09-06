32a593ffc69da3a614a9e4c59a2fdaa0786cfc6b
Jacob Homanics <homanicsjake@gmail.com>
2023-09-05 23:42:07 -0500

    cleaned up code

--- packages/nextjs/utils/scaffold-eth/contractUtils.ts
+++ packages/nextjs/utils/scaffold-eth/contractUtils.ts
@@ -3,9 +3,9 @@ import { Address } from "viem";
 import contractsData from "~~/generated/deployedContracts";
 
 // import zkContractsData from "~~/generated/zkDeployedContracts";
+// export const loadedContracts = zkContractsData as GenericContractsDeclaration | null;
 
 export const loadedContracts = contractsData as GenericContractsDeclaration | null;
-// export const loadedContracts = zkContractsData as GenericContractsDeclaration | null;
 
 type GenericContractsDeclaration = {
   [key: number]: readonly {
